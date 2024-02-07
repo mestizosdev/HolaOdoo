e9a367ed9f2dfdf99bbd4885bb069b95dd808591
Jorge Luis <jorjoluiso@gmail.com>
2024-02-07 14:37:28 -0500

    [FIX] Friend model

--- chapter_iv/custom_addons/greet/models/friend.py
+++ chapter_iv/custom_addons/greet/models/friend.py
@@ -4,11 +4,10 @@ from odoo import fields, models
 class Friend(models.Model):
     _name = 'greet.friend'
     _description = 'Friend'
-    _inherits = {"res.partner": "partner_id"}
-    _inherit = ['res.partner']
+    _inherits = {'res.partner': 'partner_id'}
 
     nickname = fields.Char()
     is_friend = fields.Boolean()
     partner_id = fields.Many2one(
-        "res.partner", delegate=True, ondelete="cascade", required=True
+        'res.partner', delegate=True, ondelete='cascade', required=True
     )
